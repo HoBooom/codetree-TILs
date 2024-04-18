1668f84c793d0b98f3f970462745f74c7d81512f
HB_Quokka <144037269+NoBrand-pizza@users.noreply.github.com>
2024-04-18 20:19:13 +0900

    Title: 정수 명령 처리, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240418/정수 명령 처리/process-numeric-commands.py
@@ -0,0 +1,39 @@
+class Stack:
+    def __init__(self):
+        self.items=[]
+
+    def push(self,item):
+        self.items.append(item)
+
+    def empty(self):
+        return not self.items
+
+    def pop(self):
+        if self.empty is True:
+            raise Exception("Stack is empty")
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def top(self):
+        if self.empth is True:
+            raise Exception("Stack is empty")
+        return self.items[-1]
+
+stack=Stack()
+iter_num=int(input())
+
+for i in range(iter_num):
+    comment=input()
+    if "push" in comment:
+        command, data=comment.split()
+        stack.push(int(data))
+    elif "size" in comment:
+        print(stack.size())
+    elif "empty" in comment:
+        print(int(stack.empty()))
+    elif "pop" in comment:
+        print(stack.pop())
+    elif "top" in comment:
+        print(stack.top())
