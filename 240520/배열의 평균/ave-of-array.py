087c5bfa1d7196f422b24e039335d28e934b3803
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2024-05-20 23:31:04 +0900

    Title: 배열의 평균, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240520/배열의 평균/ave-of-array.py
@@ -0,0 +1,21 @@
+list0=[]
+
+
+
+for i in range(2):
+    temp_list = list(map(int,input().split()))
+    list0.append(temp_list)
+
+for i in range(2):
+    print(sum(list0[i])/4,end=" ")
+print()
+
+for j in range(4):
+    column_avg=(list0[0][j] + list0[1][j]) / 2
+    print(column_avg,end=" ")
+print()
+
+sum1=sum(list0[0])
+sum2=sum(list0[1])
+
+print((sum1+sum2)/8)
