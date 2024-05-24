352c08ac93495c10ebfde441ecf2ce34bfbbab5b
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2024-05-24 18:41:39 +0900

    Title: 격자로 사각형 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240524/격자로 사각형 만들기/print-grid-in-rectangle.py
+++ 240524/격자로 사각형 만들기/print-grid-in-rectangle.py
@@ -7,7 +7,7 @@ list0=[
 
 for i in range(1,5):
     for j in range(1,5):
-        list0[i][j] = list0[i-1][j-1] + list0[i -1][j] + list0[i][j  - 1]
+        list0[i][j] = list0[i-1][j-1] + list0[i -1][j] + list0[i][j - 1]
 
 for i in range(5):
     for j in range(5):
