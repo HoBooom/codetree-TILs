057361f12e3fea6920584745e5d062a4e281af93
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2024-05-25 18:54:48 +0900

    Title: 공백을 기준으로 출력, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240525/공백을 기준으로 출력/output-based-on-space.py
@@ -0,0 +1,8 @@
+list0 = list(map(str,input().split()))
+list1 = list(map(str,input().split()))
+
+for i in range(len(list0)):
+    print(list0[i],end="")
+
+for j in range(len(list1)):
+    print(list1[j],end="")
