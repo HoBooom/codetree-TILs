6baf3a6ef2f0e59b93e094c75a88e2ab872e9cdb
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2024-05-25 20:54:52 +0900

    Title: 특정 문자로 끝나는 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240525/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
+++ 240525/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
@@ -5,7 +5,12 @@ for i in range(10):
     list0.append(arr)
 
 alpha = input()
+tf = None
 
 for i in range(10):
     if list0[i][len(list0[i]) - 1] == alpha:
         print(list0[i])
+        tf = True
+
+if !tf:
+    print(tf)
