eea692fb3b00f212b93570fe50a982c8a8d58714
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2024-05-27 17:49:53 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240527/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,23 @@
+def is_part(list0,list1,index):
+    for i in range(len(list1)):
+        if list1[i] != list0[index + i]:
+            return False
+    return True
+
+
+
+n,m = map(int,input().split())
+
+list0 = list(map(int,input().split()))
+list1 = list(map(int,input().split()))
+
+tf = False
+
+for i in range(n):
+    if list0[i]==list1[0]:
+        tf = is_part(list0,list1,i)
+
+if tf:
+    print("Yes")
+else:
+    print("No")
