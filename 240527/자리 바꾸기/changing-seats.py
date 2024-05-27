717f5f6172b23e1a2f38688fb5e72f1932686ec0
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2024-05-27 22:27:08 +0900

    Title: 자리 바꾸기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240527/자리 바꾸기/changing-seats.py
@@ -0,0 +1,66 @@
+n,k = map(int,input().split())
+
+men_in_chair = [i for i in range(n)]
+
+
+
+commend_a = []
+commend_b = []
+
+for i in range(k):
+    a,b = map(int,input().split())
+    commend_a.append(a - 1)
+    commend_b.append(b - 1)
+   
+
+men_move =[
+    [] for _ in range(n)
+]
+
+for i in range(n):
+    men_move[i].append(i)
+
+
+for i in range(k):
+    ca = commend_a[i] #1
+    cb = commend_b[i] #3
+    men_A = men_in_chair[ca] #1남
+    men_b = men_in_chair[cb] #3남
+    men_in_chair[ca],men_in_chair[cb] = men_in_chair[cb],men_in_chair[ca]
+
+    if cb not in men_move[men_A]:
+        men_move[men_A].append(cb)
+    if ca not in men_move[men_b]:
+        men_move[men_b].append(ca)
+
+
+
+for i in range(k):
+    ca = commend_a[i]
+    cb = commend_b[i]
+    men_A = men_in_chair[ca]
+    men_b = men_in_chair[cb]
+    men_in_chair[ca],men_in_chair[cb] = men_in_chair[cb],men_in_chair[ca]
+
+    if cb not in men_move[men_A]:
+        men_move[men_A].append(cb)
+    if ca not in men_move[men_b]:
+        men_move[men_b].append(ca)
+
+
+
+for i in range(k):
+    ca = commend_a[i]
+    cb = commend_b[i]
+    men_A = men_in_chair[ca]
+    men_b = men_in_chair[cb]
+    men_in_chair[ca],men_in_chair[cb] = men_in_chair[cb],men_in_chair[ca]
+
+    if cb not in men_move[men_A]:
+        men_move[men_A].append(cb)
+    if ca not in men_move[men_b]:
+        men_move[men_b].append(ca)
+
+
+for i in range(n):
+    print(len(men_move[i]))
