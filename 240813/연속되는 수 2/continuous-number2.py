bfe71d79b9eb26644da072e94ed55cc1b49a70b6
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2024-08-13 10:44:37 +0900

    Title: 연속되는 수 2, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240813/연속되는 수 2/continuous-number2.py
+++ 240813/연속되는 수 2/continuous-number2.py
@@ -8,10 +8,11 @@ count = 0
 for i in range(n):
     n = int(input())
     nums.append(n)
+    count += 1
     if i == 0 or nums[i] != nums[i - 1]:
         if count > max_fre:
             max_fre = count
         count = 0
-    count += 1
+    
     
 print(max_fre)
