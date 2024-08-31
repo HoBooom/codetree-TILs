58880a13e3b568d64aea4dd072cdeb36f664c593
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2024-08-31 13:36:02 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 5098ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 240831/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -0,0 +1,43 @@
+n = int(input())
+
+board = [
+    [0 for _ in range(n)] for _ in range(n)
+]
+
+center = n // 2
+r,c = center,center
+
+drs = [0,-1,0,1]
+dcs = [1,0,-1,0]
+direct = 0
+
+cnt = 1
+walk = 1
+
+board[r][c] = cnt
+
+
+def is_range(r,c):
+    return 0<=r<n and 0<=c<n
+
+check = 0
+temp = 0
+while cnt < n**2:
+    if temp == walk:
+        direct = (direct + 1) % 4
+        temp = 0
+        check += 1
+        if check == 2:
+            walk += 1
+    
+    nr,nc = r + drs[direct],c + dcs[direct]
+    if is_range(nr,nc) and board[nr][nc] == 0:
+        r,c = nr,nc
+        cnt += 1
+        board[r][c] = cnt
+        temp += 1 
+
+for i in range(n):
+    for j in range(n):
+        print(board[i][j],end = " ")
+    print()
