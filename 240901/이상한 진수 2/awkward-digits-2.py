d855e4349cb878294744920002807d33b8b441bd
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2024-09-01 18:08:11 +0900

    Title: 이상한 진수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240901/이상한 진수 2/awkward-digits-2.py
+++ 240901/이상한 진수 2/awkward-digits-2.py
@@ -2,13 +2,18 @@ bin_num = list(input())
 
 int_num = 0
 
-for i,v in enumerate(bin_num):
-    if v == '0':
-        bin_num[i] = '1'
-        break
+def f(bin_num):
+    if '0' not in bin_num:
+        bin_num[-1] = '0'
+    else:
+        for i,v in enumerate(bin_num):
+            if v == '0':
+            bin_num[i] = '1'
+            break
+    return bin_num
+bin_num = f(bin_num)
 
-if '0' not in bin_num:
-    bin_num[-1] = '0'
+
 
 for i,v in enumerate(reversed(bin_num)):
     int_num += int(v)*(2**i)
