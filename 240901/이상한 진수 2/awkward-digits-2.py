7ac6a0fd8c700a8c427bccdac7693e96f7b653b9
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2024-09-01 18:42:29 +0900

    Title: 이상한 진수 2, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240901/이상한 진수 2/awkward-digits-2.py
+++ 240901/이상한 진수 2/awkward-digits-2.py
@@ -5,11 +5,12 @@ int_num = 0
 def f(bin_num):
     if '0' not in bin_num:
         bin_num[-1] = '0'
-    else:
+    elif '0' in bin_num:
         for i,v in enumerate(bin_num):
             if v == '0':
                 bin_num[i] = '1'
-            break
+                break
+    #print('f',bin_num)
     return bin_num
 
 bin_num = f(bin_num)
