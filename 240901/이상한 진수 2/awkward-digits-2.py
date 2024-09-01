df91f12cab7ab0e4b24d533a7814a7668c1531d0
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2024-09-01 18:06:54 +0900

    Title: 이상한 진수 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240901/이상한 진수 2/awkward-digits-2.py
+++ 240901/이상한 진수 2/awkward-digits-2.py
@@ -7,8 +7,8 @@ for i,v in enumerate(bin_num):
         bin_num[i] = '1'
         break
 
-if bin_num == ['1']:
-    bin_num = ['0']
+if '0' not in bin_num:
+    bin_num[-1] = '0'
 
 for i,v in enumerate(reversed(bin_num)):
     int_num += int(v)*(2**i)
