c6850b8bd8cc7a3061dd2f941de52960024a9614
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2024-09-04 10:06:31 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240904/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240904/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -3,19 +3,20 @@ command = list(input())
 count = 0
 
 count_front = 0
-front_index = False
 
-count_back = 0
+
+def f(i,command):
+    count_back = 0
+    for j in range(i+1,len(command) - 1):
+        if command[j]+command[j+1] == "))":
+            count_back += 1
+    return count_back
 
 for i in range(len(command) - 1):
     context = command[i]+command[i+1]
-    #print(context)
-    if  context == "((":
-        count_front += 1
-        #print(count_front,"front")
-        front_index = True
-    elif context == "))" and front_index == True:
-        count_back += 1
-       # print(count_back,"back")
+    if context == "((":
+        count += f(i,command)
+    
+    
 
-print(count_front * count_back)
+print(count)
