040ac47b2302bd076363fc49622b6689c2e58165
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2024-09-12 22:10:05 +0900

    Title: 아름다운 수열 2, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240912/아름다운 수열 2/beautiful-sequence-2.py
+++ 240912/아름다운 수열 2/beautiful-sequence-2.py
@@ -3,18 +3,37 @@ n,m = map(int,input().split())
 nums_a = list(map(int,input().split()))
 nums_b = list(map(int,input().split()))
 
-def is_in_list(temp_list):
+def is_in_list(temp_list,check_b):
+    
+
     for i in range(len(temp_list)):
         if temp_list[i] not in nums_b:
             return False
+
+        for i,v in enumerate(check_b):
+            if v[0] == temp_list[i] and v[1] == False:
+                v[1] = True
+                break
+        
+    for i,v in enumerate(check_b):
+        if v[1] == False:
+            return False
+
     return True
 
 count = 0
 
+check_b = []
+for i in range(m):
+    check_b.append([nums_b[i],False])
+#print(check_b)
+
+
 for i in range(n - m + 1):
     temp_list = nums_a[i:i + m]
     
-    if is_in_list(temp_list):
+    
+    if is_in_list(temp_list,check_b):
         count += 1
 
 print(count)
