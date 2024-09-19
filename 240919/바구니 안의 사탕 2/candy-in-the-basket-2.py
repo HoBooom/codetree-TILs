3abde4225af12345cc91c437fb873a4c44df6e75
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2024-09-19 14:19:16 +0900

    Title: 바구니 안의 사탕 2, Time: 130ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240919/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240919/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -15,13 +15,28 @@ baskets.sort(key = lambda x : x[1])
 first_place = baskets[0][1]
 last_place = baskets[n - 1][1] # 15
 
+
 ans = INT_MIN
 
 
 for i in range(first_place + k, last_place - k + 1):
     temp = 0
+    f_index = i - k
+    l_index = i + k
+    if f_index < 0:
+        f_index = 0
+    if l_index > 100:
+        l_index = 100
+
+    
     for j in range(n):
-        if i - k <= baskets[j][1] <= i + k:
+        if f_index <= baskets[j][1] <= l_index:
             temp += baskets[j][0]
     ans = max(ans,temp)
+
+if first_place == last_place:
+    ans = 0
+    for i in range(n):
+        ans = baskets[i][0]
+
 print(ans)
