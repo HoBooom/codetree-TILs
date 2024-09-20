65fa445707b91b12fe07e0998ca350f25eabd1c8
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2024-09-20 11:05:42 +0900

    Title: 밭의 높이를 고르게하기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240920/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240920/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -4,11 +4,10 @@ INT_MAX = sys.maxsize
 
 n,h,t = map(int,input().split())
 area = list(map(int,input().split()))
-area.sort()
 
 cost = INT_MAX
 
-
+#print(area)
 def check(area):
     temp = 0
     for i,v in enumerate(area):
@@ -20,6 +19,7 @@ for i in range(n - t):
     temp = 0
     for j in range(i,i + t):
         temp += abs(area[j] - h)
+    #print(cost,temp)
     cost=min(cost,temp)
 
 print(cost)
