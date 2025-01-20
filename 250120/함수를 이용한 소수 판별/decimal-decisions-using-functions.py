3fbd7c30926a006743619760e2721e6d626a9b80
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2025-01-20 15:14:38 +0900

    Title: 함수를 이용한 소수 판별, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250120/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,17 @@
+a, b = map(int, input().split())
+
+# Write your code here!
+
+def f(n):
+    for j in range(2,n):
+        if n % j == 0:
+            #print(n)
+            return False
+    return True
+
+ans = 0
+for i in range(a, b + 1):
+    if f(i):
+        ans += i
+
+print(ans)
