bbe87e17c8721380fe884035b2e126d09ec5b366
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2025-01-20 10:25:11 +0900

    Title: 최소공배수 구하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250120/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+
+# Write your code here!
+
+ans = n * m
+
+m_cnt = 1
+
+for i in range(1,n):
+    #print(f"now {i}번째 반복, n1 {i*n}, n2 {m*m_cnt}")
+    if i*n < m * m_cnt:
+        #print(f"now n1 {i*n}, n2 {m*m_cnt}")
+        continue
+    elif i*n > m * m_cnt:
+        while m * (m_cnt) < i * n:
+            m_cnt += 1
+            #print(f"now n1 {i*n}, n2 {m*m_cnt}")
+    elif i*n == m*m_cnt:
+        ans = i*n
+        break
+
+print(ans)
+        
