de1924e8d198d220705e561807f28296d76cba68
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2025-01-23 09:28:02 +0900

    Title: 두 정수에 대한 연산값 2, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250123/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -0,0 +1,16 @@
+a, b = map(int, input().split())
+
+# Write your code here!
+
+def f(a,b):
+    if a > b:
+        a *= 2
+        b += 10
+    else:
+        a += 10
+        b *= 2
+    return a,b
+
+a,b = f(a,b)
+print(a,b)
+    
