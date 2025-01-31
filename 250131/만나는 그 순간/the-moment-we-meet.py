ef15db1a89966ae0aee160734d1f7cba6d0d50c0
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2025-01-31 16:33:43 +0900

    Title: 만나는 그 순간, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250131/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,45 @@
+n,m = map(int,input().split())
+
+
+a = []
+b = []
+a.append(0)
+b.append(0)
+
+cnt_a = 0
+cnt_b = 0
+
+for _ in range(n):
+    dr,xa = input().split()
+    xa= int(xa)
+    if dr == 'R':
+        for _ in range(xa):
+            cnt_a += 1
+            a.append(cnt_a)
+    elif dr =='L':
+        for _ in range(xa):
+            cnt_a -= 1
+            a.append(cnt_a)
+
+for _ in range(m):
+    dr,xb = input().split()
+    xb = int(xb)
+    if dr =='R':
+        for _ in range(xb):
+            cnt_b += 1
+            b.append(cnt_b)
+    elif dr =='L':
+        for _ in range(xb):
+            cnt_b -= 1
+            b.append(cnt_b)
+
+#print(a)
+#print(b)
+
+for i,item in enumerate(a):
+    if item == b[i] and i != 0:
+        print(i)
+        break
+            
+
+    
