5209bfbd02a5635baeb01dc314ffd1a1f86487bc
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2025-02-01 18:16:59 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250201/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 250201/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -8,15 +8,23 @@ handshakes.sort(key = lambda x:x[0])
 
 man = [0]*N
 man[P - 1] = 1
-chances = [2]*N
+chances = [K]*N
 
 for _,(t,x,y) in enumerate(handshakes):
+    temp = False
+    if man[y-1] != 1:
+        temp = True
     if man[x - 1] == 1 and chances[x - 1] > 0:
         man[y - 1] = 1
         chances[x - 1] -= 1
+        if temp:
+            continue
     if man[y - 1] == 1 and chances[y - 1] > 0:
         man[x - 1] = 1
         chances[y - 1] -= 1
+
+#for _,item in enumerate(handshakes):
+#    print(item)
 
 for i,m in enumerate(man):
     print(m,end="")
