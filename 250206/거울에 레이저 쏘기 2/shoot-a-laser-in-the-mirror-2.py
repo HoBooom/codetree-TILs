f2c762445ed91d13e00894b7253bca2212a0b289
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2025-02-06 14:45:26 +0900

    Title: 거울에 레이저 쏘기 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250206/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 250206/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -1,7 +1,6 @@
 n = int(input())
 grid = [list(input()) for _ in range(n)]
 cnt = int(input())
-cnt -= 1
 
 # Write your code here!
 
@@ -29,17 +28,13 @@ def first_position(cnt):
     r,c = 0,0
     dr,dc = [0,1,0,-1],[1,0,-1,0]
     dir_num = 0
-    #print(f'first_position {cnt}')
-    for i in range(cnt):
-        #print(i)
+    for i in range(0,cnt):
         if i == 0:
             None
-        elif i % 3 == 0:
+        elif i % n == 0:
             dir_num += 1
-            #print('dir change')
-            continue
-        r,c = r + dr[dir_num],c + dc[dir_num]
-        #print(r,c)
+        else:
+            r,c = r + dr[dir_num],c + dc[dir_num]
     return r,c
         
 
@@ -51,17 +46,25 @@ def is_range(r,c):
 cnt_r,cnt_c = first_position(cnt)
 
 ans = 0
-input_dir = cnt // 3
+input_dir = (cnt - 1) // n
 
 while True:
     if is_range(cnt_r,cnt_c):
-        
         output_dir = razer(grid[cnt_r][cnt_c],input_dir)
         ans += 1
-        dr,dc = [0,1,0,-1],[1,0,-1,0]
+        dr,dc = [-1,0,1,0],[0,1,0,-1]
+        #print(f'cnt {grid[cnt_r][cnt_c]}, input {input_dir} output {output_dir} before {cnt_r} {cnt_c}',end= " ")
         cnt_r,cnt_c = cnt_r + dr[output_dir],cnt_c + dc[output_dir]
+        #print(f'next {cnt_r} {cnt_c}')
         # 1 => 3,0 => 2, 2 => 0, 3 => 1
-        input_dir = razer('\\',output_dir)
+        if output_dir == 0:
+            input_dir = 2
+        elif output_dir == 2:
+            input_dir = 0
+        elif output_dir == 1:
+            input_dir = 3
+        elif output_dir == 3:
+            input_dir = 1
     else:
         break
 
