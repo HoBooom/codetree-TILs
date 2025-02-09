94b81047c0a5dea3de39ebfcb84f04878fce8194
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2025-02-09 18:01:49 +0900

    Title: 체크판위에서 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250209/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,44 @@
+R, C = map(int, input().split())
+grid = [list(input().split()) for _ in range(R)]
+
+# Write your code here!
+
+ans = 0
+
+cnt_r,cnt_c = 0,0
+
+def find_first_rc(color):
+    find = []
+    for r in range(R-2):
+        for c in range(C - 2):
+            if color == 'W':
+                if grid[r][c] == 'B':
+                    find.append((r,c))
+            elif color == 'B':
+                if grid[r][c] == 'W':
+                    find.append((r,c))
+    return find
+
+def find_sec_rc(row,column,color):
+    find = []
+    for r in range(row + 1,R - 1):
+        for c in range(column + 1,C - 1):
+            if color == 'W':
+                if grid[r][c] == 'B':
+                    find.append((r,c))
+            elif color == 'B':
+                if grid[r][c] == 'W':
+                    find.append((r,c))
+    return find
+
+
+first_points = find_first_rc(grid[0][0])
+
+
+for _,(r,c) in enumerate(first_points):
+    second_points = find_sec_rc(r,c,grid[r][c])
+    ans += len(second_points)
+
+print(ans)
+
+
