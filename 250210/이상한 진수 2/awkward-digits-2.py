3e0e5156042a0a6529aa6430b14680fa3ed407ae
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2025-02-10 11:15:02 +0900

    Title: 이상한 진수 2, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250210/이상한 진수 2/awkward-digits-2.py
+++ 250210/이상한 진수 2/awkward-digits-2.py
@@ -5,19 +5,17 @@ ans = 0
 
 if '0' not in a:
     a.pop()
-    a.append(0)
+    a.append('0')
 else:
     for i in range(len(a)):
         if a[i] == '0':
             a[i] = '1'
-            #print(i,a)
             break
-        #print(i,a)
 
-#print(a)
-if len(a) > 0:
-    a = ''.join(a)
-#print(a)
-ans = int(a,2)
+
+a = ''.join(a)
+if len(a)>0:
+    ans = int(a,2)
+
 
 print(ans)
