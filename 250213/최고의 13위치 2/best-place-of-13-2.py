56c20d1789d64aeae984b9ce47b711d6d9301a42
HBChoi <144037269+NoBrand-pizza@users.noreply.github.com>
2025-02-13 15:40:56 +0900

    Title: 최고의 13위치 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250213/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,22 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+###
+ans = 0
+
+for r in range(n):
+    for c in range(n - 2):
+        block = arr[r][c] + arr[r][c + 1] + arr[r][c + 2]
+        for row in range(n):
+            for col in range(n - 2):
+                if r == row and c <= col <= c + 2:
+                    continue
+                block2 = arr[row][col] + arr[row][col + 1] + arr[row][col + 2]
+                if block + block2 > ans:
+                    ans = block + block2
+
+print(ans)
+
+        
+
+
