143bdf09ebb325eebff13bd2270d40065c9a895e
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-02-21 02:27:41 +0900

    Title: 스승의 은혜 3, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250221/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250221/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -5,20 +5,31 @@ stu = [list(map(int,input().split())) for _ in range(n)]
 ans = 0
 for i in range(n):
     budget = 0
-    temp = stu[:]
-    temp[i][0] = temp[i][0]//2
-    temp.sort(key = lambda x:x[0])
-    #print(temp)
     students = 0
+    new_list = []
     for j in range(n):
-        budget += (temp[j][0] + temp[j][1])
+        if i == j:
+            new_list.append(stu[j][0]//2 + stu[j][1])
+        else:
+            new_list.append(stu[j][0] + stu[j][1])
+    #print(new_list)
+    new_list.sort()
+    #print(new_list)
+    #print()
+    
+    
+    for j in range(n):
+        budget += (new_list[j])
+        #print(budget,end = " ")
         if budget > b:
-            budget -= (temp[j][0] + temp[j][1])
+            break
         else:
             students += 1
-    if students > ans:
+    #print("stu",students)
+    if ans <= students:
         ans = students
+        #print(ans)
 
-print(students)
+print(ans)
     
         
