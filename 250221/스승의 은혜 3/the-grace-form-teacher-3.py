77875391709fc09469a529f56d4d8192a3238867
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-02-21 02:00:33 +0900

    Title: 스승의 은혜 3, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250221/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,22 @@
+n,b = map(int,input().split())
+
+stu = [list(map(int,input().split())) for _ in range(n)]
+
+ans = 0
+for i in range(n):
+    budget = 0
+    temp = stu[:]
+    temp[i][0] = temp[i][0]//2
+    temp.sort(key = lambda x:x[0])
+    students = 0
+    for j in range(n):
+        budget += (temp[j][0] + temp[j][1])
+        if budget > b:
+            break
+        students += 1
+    if students > ans:
+        ans = students
+
+print(students)
+    
+        
