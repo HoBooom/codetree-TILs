bf9b96dc054cd7be92d221c9c31854182bf70828
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-02-24 23:12:03 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250224/연속 부분 합의 최댓값 구하기/max-of-partial-sum.py
+++ 250224/연속 부분 합의 최댓값 구하기/max-of-partial-sum.py
@@ -1,14 +1,27 @@
+import sys
+
+INT_MIN = -sys.maxsize
+
 n = int(input())
 arr = list(map(int, input().split()))
 
 # Write your code here!
 
-ans = min(arr)
+prefix_list = []
 
+def pre_min(i):
+    temp = - INT_MIN
+    for i in range(i):
+        if prefix_list[i] < temp:
+            temp = prefix_list[i]
+    return temp
+ans = INT_MIN
+temp = 0
 for i in range(len(arr)):
-    temp = 0
-    for j in range(i,len(arr)):
-        temp += arr[j]
-        ans = max(ans,temp)
+    temp += arr[i]
+    prefix_list.append(temp)
+
+for i in range(len(prefix_list)):
+    ans = max((prefix_list[i] - pre_min(i)),ans)
 
 print(ans)
