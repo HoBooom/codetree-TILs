c533957c66e524053b22fac82c547eefc7d738ef
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-02-24 20:36:51 +0900

    Title: 팀으로 하는 틱택토 2, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250224/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 250224/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,19 +1,16 @@
 inp = [list(input()) for _ in range(3)]
 
 # Write your code here!
-def is_range(r,c):
-    if 0<=r<3 and 0<=c<3:
-        return True
-    return False
+ans_set = []
 
 def is_win(lis):
     temp = set()
     for _,item in enumerate(lis):
         temp.add(item)
     if len(temp) == 2:
+        ans_set.append(temp)
         return True
     return False
-
 ans = 0
 
 for r in range(3):
@@ -34,4 +31,11 @@ if is_win(temp):
 temp1 = [inp[0][2],inp[1][1],inp[2][0]]
 if is_win(temp1):
     ans +=1 
-print(ans)
+
+
+anss = []
+for item in ans_set:
+    if item not in anss:
+        anss.append(item)
+
+print(len(anss))
