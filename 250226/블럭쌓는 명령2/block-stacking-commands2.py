6b131da44f31529d7b7bb86160f5e39e850e9e7d
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-02-26 20:21:28 +0900

    Title: 블럭쌓는 명령2, Time: 69ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250226/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,10 @@
+n, k = map(int,input().split())
+
+block = [0] * n
+
+for _ in range(k):
+    x0,x1 = map(int,input().split())
+    for i in range(x0,x1 + 1):
+        block[i] += 1
+
+print(max(block))
