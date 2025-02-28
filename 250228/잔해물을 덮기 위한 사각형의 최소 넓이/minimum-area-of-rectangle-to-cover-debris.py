0213ec834a6beab50ae652685e4a49eeebdd7f65
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-02-28 18:24:58 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 203ms, Memory: 49MB, Status: Passed - CodeTree

--- 250228/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250228/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -2,13 +2,13 @@
 board = [[0 for _ in range(2030)] for _ in range(2030)]
 
 def color(x1,y1,x2,y2):
-    for x in range(x1,x2 + 1):
-        for y in range(y1,y2 + 1):
+    for x in range(x1,x2):
+        for y in range(y1,y2):
             board[y][x] += 1
 
 def erase(x1,y1,x2,y2):
-    for x in range(x1,x2 + 1):
-        for y in range(y1,y2 + 1):
+    for x in range(x1,x2):
+        for y in range(y1,y2):
             board[y][x] -= 1
 
 
@@ -25,6 +25,8 @@ max_x = 0
 min_x = 2030
 max_y = 0
 min_y = 2030
+isChange = False
+
 
 for x in range(2030):
     for y in range(2030):
@@ -33,10 +35,11 @@ for x in range(2030):
             min_x = min(x,min_x)
             max_y = max(y,max_y)
             min_y = min(y,min_y)
+            isChange = True
 
-#print(max_x,max_y,min_x,min_y)
-ans = (abs(max_x - min_x)  * abs(max_y - min_y))
-if ans == 4120900:
+
+ans = ((abs(max_x - min_x)+1) * (abs(max_y - min_y) + 1))
+if not isChange:
     print(0)
 else:
     print(ans)
