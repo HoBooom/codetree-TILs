8dfa5c8b80c9fd53c23e629421656c93e3c899cc
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-05 10:20:55 +0900

    Title: 언덕 깎기, Time: 71ms, Memory: 17MB, Status: Passed - CodeTree

--- 250305/언덕 깎기/hill-cutting.py
+++ 250305/언덕 깎기/hill-cutting.py
@@ -1,34 +1,21 @@
+import sys
+
+INT_MAX = sys.maxsize
+MAX_H = 100
+
 n = int(input())
+k = 17
+hill = [int(input()) for _ in range(n)]
 
-hills = [int(input()) for _ in range(n)]
+ans = INT_MAX
 
-hills.sort()
+for i in range(MAX_H):
+    cost = 0
+    for j in range(n):
+        if hill[j] < i:
+            cost += (hill[j] - i) ** 2
+        if hill[j] > i + k:
+            cost += (hill[j] - (i + k)) ** 2
+    ans = min(ans,cost)
 
-cost = 0
-
-def check(hill1,hill2):
-    if 0 <= hill2 - hill1 <=17:
-        return True
-    return False 
-
-def f(hill1,hill2):
-    temp1 = hill1
-    temp2 = hill2
-    while True:
-        if temp2 - temp1 <= 17:
-            break
-        temp1 += 1
-        temp2 -= 1
-    return temp1,temp2
-    
-length = n - 1
-
-for i in range(length): 
-    if check(hills[i], hills[length - i]):
-        break
-    t1,t2 = f(hills[i], hills[length - i])
-    cost += ((t1 - hills[i]) ** 2) + ((hills[length - i] - t2) ** 2)
-
-print(cost)
-
-    
+print(ans)
