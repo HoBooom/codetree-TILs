04b3e1c9bb3e8b44b86a62d17747cb6516c5c12e
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-05 18:32:06 +0900

    Title: 구간 잘 나누기, Time: 77ms, Memory: 17MB, Status: Passed - CodeTree

--- 250305/구간 잘 나누기/divide-sections-well.py
+++ 250305/구간 잘 나누기/divide-sections-well.py
@@ -1,33 +1,28 @@
-import sys
+MAX_INT = 10000
 
-INT_MAX = sys.maxsize
+n,m = map(int,input().split())
 
-n, m = map(int,input().split())
+arr = list(map(int,input().split()))
 
-nums = list(map(int,input().split()))
+ans = MAX_INT
 
-blank = []
+for i in range(1,MAX_INT):
+    cnt_sum = 0
+    section = 1
 
-for i in range(n - 1):
-    blank.append(nums[i])
-    blank.append(0)
-blank.append(nums[-1])
-
-ans = INT_MAX
-
-for i in range(n + (n - 2)):
-    for j in range(i + 1, n + (n - 2)):
-        #print(i,j)
-        if i % 2 == 0 or j % 2 == 0:
-            continue
-        temp1 = (blank[:i])
-        temp2 = (blank[i + 1:j])
-        temp3 = (blank[j + 1:])
-        temp = max(sum(temp1),sum(temp2),sum(temp3))
-        #print(temp1,temp2,temp3)
-        ans = min(ans,temp)
-
+    is_ok = True
+    for j in range(n):
+        if arr[j] > i:
+            is_ok = False
+            break
+        cnt_sum += arr[j]
+        if cnt_sum > i:
+            section += 1
+            cnt_sum = arr[j]
+    if section <= m and is_ok:
+        ans = min(ans,i)
 
 print(ans)
 
-            
+        
+
