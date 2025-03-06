10e65c938e2560d4ba8f91b665dc5f04faf76493
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-06 15:35:39 +0900

    Title: 바구니 안의 사탕 2, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250306/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,24 @@
+N, K = map(int, input().split())
+candy_box = [0] * 101
+
+for _ in range(N):
+    c, p = map(int, input().split())
+    candy_box[p] += c
+
+ans = 0
+
+
+for i in range(len(candy_box)):
+    first = i - K
+    end = i + K
+    if first < 0:
+        first = 0
+    if end > 100:
+        end = 100
+    temp = sum(candy_box[first:end + 1])
+    if temp > ans:
+        #print(first,end,temp)
+        ans = temp
+
+print(ans)
+    
