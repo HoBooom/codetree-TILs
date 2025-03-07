bb027cb27214f07ae63aeb6c338e8f32a86d4856
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-07 16:29:26 +0900

    Title: 숫자 카운트, Time: 70ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 250307/숫자 카운트/numeric-count.py
@@ -0,0 +1,47 @@
+n = int(input())
+
+nums = []
+
+for i in range(1,9 + 1):
+    for j in range(1,9 + 1):
+        for k in range(1,9 + 1):
+            if j != i:
+                if k != i and k != j:
+                    num = (i*100 + j*10 + k)
+                    nums.append(list(str(num)))
+
+def count1(num1,num2): #num1이 정답, num2가 체크
+    count = 0
+    for i in range(3):
+        if num1[i] == num2[i]:
+            count += 1
+    return count
+
+def count2(num1,num2):
+    count = 0
+    for i in range(3):
+        if num1[i] != num2[i] and (num2[i] in num1):
+            count += 1
+    return count
+
+for _ in range(n):
+    num,c1,c2 = input().split()
+    num = list(num)
+    c1 = int(c1)
+    c2 = int(c2)
+    for i in range(len(nums)):
+        if count1(nums[i],num) != c1 or count2(nums[i],num) !=c2:
+            nums[i] = '000'
+
+ans = 0
+
+for _,item in enumerate(nums):
+    if item != '000':
+        ans += 1
+
+print(ans)
+    
+
+
+
+
