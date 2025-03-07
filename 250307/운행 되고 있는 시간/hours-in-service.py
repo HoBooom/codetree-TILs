cc0a67fe0c8e31ddc20b5b0e2498ffa6eef0bbeb
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-07 17:12:10 +0900

    Title: 운행 되고 있는 시간, Time: 57ms, Memory: 17MB, Status: Passed - CodeTree

--- 250307/운행 되고 있는 시간/hours-in-service.py
+++ 250307/운행 되고 있는 시간/hours-in-service.py
@@ -1,21 +1,16 @@
 n = int(input())
 order = [tuple(map(int,input().split())) for _ in range(n)]
-time = [0] * 1001
 
-for _,(a,b) in enumerate(order):
-    for i in range(a,b):
-        time[i] += 1
 
 ans = 0
 
 for i in range(n):
-    cnt_time = 0
-    a,b = order[i]
-    for j in range(a,b + 1):
-        time[j] -= 1
-    cnt_time = sum(time) 
-    ans = max(ans,cnt_time)
-    for j in range(a,b + 1):
-        time[j] += 1
+    cnt_time = [0] * 1001
+    cnt_order = order[:i] + order[i + 1:]
+    for a,b in cnt_order:
+        for j in range(a,b):
+            cnt_time[j] = 1
+    ans = max(ans,sum(cnt_time))
 
+    
 print(ans)
