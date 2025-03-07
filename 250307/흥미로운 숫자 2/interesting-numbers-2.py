2e8a967ea28a93a431166980581429d65746475c
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-07 19:11:46 +0900

    Title: 흥미로운 숫자 2, Time: 172ms, Memory: 17MB, Status: Passed - CodeTree

--- 250307/흥미로운 숫자 2/interesting-numbers-2.py
+++ 250307/흥미로운 숫자 2/interesting-numbers-2.py
@@ -1,29 +1,17 @@
 x,y = map(int,input().split())
 
 def f(n):
-    n = str(n)
-    n = list(n)
+    n = list(str(n))
     num_list = [0]*10
 
     for i in range(len(n)):
         num_list[int(n[i])] += 1
     count = 0
-    for i in range(len(num_list)):
-        if num_list[i] >= 1:
-            count += 1
-    #print(num_list)
-    if count >= 3:
-        return False
-    if count == 2:
-        t = 0
-        for i in range(len(num_list)):
-            if num_list[i] >= 2:
-                t += 1
-        if t == 2:
-            return False
-        elif t == 1:
-            return True
-    return False
+    is_interesting = False
+    for i in range(10):
+        if num_list[i] == len(n) - 1:
+            is_interesting = True
+    return is_interesting
 
 ans = 0
 for i in range(x,y + 1):
