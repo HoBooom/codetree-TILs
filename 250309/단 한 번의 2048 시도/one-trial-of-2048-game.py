1bf99026a20b48e793e69cd4f47b71ba3ebfedd0
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-09 14:13:12 +0900

    Title: 단 한 번의 2048 시도, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250309/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -0,0 +1,62 @@
+board = [list(map(int,input().split())) for _ in range(4)]
+
+direct = input()
+
+def shift(arr):
+    temp = []
+    for i in range(4):
+        if arr[i] != 0:
+            temp.append(arr[i])
+    
+    temp = temp + ([0] * (4 - len(temp)))
+    return temp
+
+def boom(arr):
+    for i in range(1,4):
+        if arr[i] == arr[i - 1]:
+            arr[i - 1] = arr[i - 1] * 2
+            arr[i] = 0
+    return arr
+
+
+if direct == 'R':
+    for i in range(4):
+        temp_arr = list(reversed(board[i]))
+        temp_arr = shift(temp_arr)
+        temp_arr = boom(temp_arr)
+        temp_arr = shift(temp_arr)
+        board[i] = list(reversed(temp_arr))
+elif direct == 'L':
+    for i in range(4):
+        temp_arr = shift(board[i])
+        temp_arr = boom(temp_arr)
+        temp_arr = shift(temp_arr)
+        board[i] = temp_arr
+elif direct == 'U':
+    for c in range(4):
+        temp_arr = []
+        for r in range(4):
+            temp_arr.append(board[r][c])
+        temp_arr = shift(temp_arr)
+        temp_arr = boom(temp_arr)
+        temp_arr = shift(temp_arr)
+        for r in range(4):
+            board[r][c] = temp_arr[r]
+elif direct == 'D':
+    for c in range(4):
+        temp_arr = []
+        for r in reversed(range(4)):
+            temp_arr.append(board[r][c])
+        temp_arr = shift(temp_arr)
+        temp_arr = boom(temp_arr)
+        temp_arr = shift(temp_arr)
+        for i in range(4):
+            board[3 - i][c] = temp_arr[i]
+
+for r in range(4):
+    print(*board[r])
+
+
+
+
+
