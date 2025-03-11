1234f4e5d9a5cce1315623f5edbd5967ec49673a
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-12 00:56:59 +0900

    Title: 이상한 폭탄 3, Time: 58ms, Memory: 17MB, Status: Passed - CodeTree

--- 250312/이상한 폭탄 3/strange-bomb-3.py
+++ 250312/이상한 폭탄 3/strange-bomb-3.py
@@ -1,34 +1,21 @@
+from collections import Counter
+
 n, k = map(int,input().split())
 
 boom = [int(input()) for _ in range(n)]
 
-max_count = -1
+max_count = - 1
 ans = 0
 
 for i in range(n - k):
-    temp = boom[i : i + k + 1]
-    temp.sort()
-    count = 1
-    for j in range(1,len(temp)):
-        if temp[j] == temp[j - 1]:
-            count += 1
-            if j == len(temp) - 1 and count >= 2:
-                if count > max_count:
-                    max_count = count
-                    ans = temp[j - 1]
-                elif count == max_count:
-                    ans = max(ans,temp[j - 1])   
-        else:
-            if count >= 2:
-                if count > max_count:
-                    max_count = count
-                    ans = temp[j - 1]
-                elif count == max_count:
-                    ans = max(ans,temp[j - 1])            
-            count = 1
+    cnt_boom = boom[i:i + k + 1]
+    boom_counter = Counter(cnt_boom)
+
+    for boom_number, count in boom_counter.items():
+        if count >= 2 and count > max_count:
+            max_count = count
+            ans = boom_number
+        elif count == max_count:
+            ans = max(ans,boom_number)
 
 print(ans)
-
-    
-
-
