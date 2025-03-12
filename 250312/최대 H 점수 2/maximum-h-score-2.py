64f4b947da57c793c4d62866e8321f84842fce6e
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-12 09:41:54 +0900

    Title: 최대 H 점수 2, Time: 62ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250312/최대 H 점수 2/maximum-h-score-2.py
@@ -0,0 +1,38 @@
+from collections import Counter
+
+N,L = map(int,input().split())
+
+num_list = list(map(int,input().split()))
+
+num_counter = Counter(num_list)
+
+def bigger_than_num(cnt_num):
+    count = 0
+    for num,count_n in num_counter.items():
+        if num > cnt_num:
+            count += count_n
+    return count
+
+ans = 0
+
+for i in range(N):
+    is_num = False
+    cnt_num = num_list[i]
+    count_sum = bigger_than_num(cnt_num) + num_list.count(cnt_num)
+
+    cnt_num_minus_one = num_list.count(cnt_num - 1)
+    if cnt_num_minus_one >= L:
+        count_sum += L
+    else:
+        count_sum += cnt_num_minus_one
+    
+    if count_sum >= cnt_num:
+        ans = max(ans,cnt_num)
+
+print(ans)
+        
+    
+
+
+
+    
