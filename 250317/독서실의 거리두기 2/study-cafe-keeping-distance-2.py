320a4c2da35a45056a1be4edb597530624b70d15
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-17 13:40:11 +0900

    Title: 독서실의 거리두기 2, Time: 67ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250317/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -0,0 +1,36 @@
+import sys
+
+INT_MAX = sys.maxsize
+
+n = int(input())
+
+place = list(input())
+
+def min_dis(place):
+    temp_distance = 0
+    min_dis = INT_MAX
+    
+    cnt_start = -1
+    for i in range(n):
+        if place[i] == "1":
+            if cnt_start == -1:
+                cnt_start = i
+                continue
+            min_dis = min(min_dis,temp_distance)
+            temp_distance = 0
+        else:
+            temp_distance += 1
+    return min_dis
+
+ans = 0
+        
+for i in range(n):
+    if place[i] == "0":
+        place[i] = "1"
+        cnt_dis = min_dis(place) + 1
+        ans = max(ans,cnt_dis)
+        #print(place, cnt_dis)
+        place[i] = "0"
+
+print(ans)
+    
