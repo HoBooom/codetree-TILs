8f905f4219e2d036b91f81b0c76019a3ed6de9d2
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-18 23:02:51 +0900

    Title: 세 수의 최대 곱, Time: 67ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250318/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+nums = list(map(int,input().split()))
+nums.sort()
+
+neg_nums = []
+pos_nums = []
+
+for i in range(n):
+    if nums[i] > 0:
+        pos_nums.append(nums[i])
+    elif nums[i] < 0:
+        neg_nums.append(nums[i])
+
+ans = 0
+
+if len(neg_nums) >= 2:
+    if len(pos_nums) >= 3:
+        t1 = pos_nums[-1] * pos_nums[-2] * pos_nums[-3]
+        t2 = neg_nums[0] * neg_nums[1] * pos_nums[-1]
+        ans = max(t1,t2)
+    else:
+        ans = pos_nums[-1] * pos_nums[-2] * pos_nums[-3]
+else:
+    ans = pos_nums[-1] * pos_nums[-2] * pos_nums[-3]
+print(ans)
+
