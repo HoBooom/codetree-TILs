709a4a73e7aa07c3d529c8afdc4f3fe24d8ba5af
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-19 09:31:18 +0900

    Title: 최소 와이파이 수, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250319/최소 와이파이 수/minimum-number-of-wifi.py
+++ 250319/최소 와이파이 수/minimum-number-of-wifi.py
@@ -3,23 +3,15 @@ man,wifi = map(int,input().split())
 mans = list(map(int,input().split()))
 
 wifi_area = 2 * wifi + 1
-ans = 0
+ans = cnt_idx = 0
 
-temp_count = 0
-for i in range(man):
-    if mans[i] == 1 and temp_count == 0:
+while cnt_idx < man:
+    if mans[cnt_idx] == 1:
         ans += 1
-        temp_count += 1
-    elif temp_count >= 1:
-        temp_count += 1
+        cnt_idx += wifi_area
+    else:
+        cnt_idx += 1
 
-    if temp_count > wifi_area:
-        if mans[i] == 1:
-            ans += 1
-            temp_count = 1
-        else:
-            temp_count = 0
-    #print(f"idx : {i}, cnt_wifi_count : {ans}, temp_count : {temp_count}")
 print(ans)
 
 
