82c778415bee488ffa64eeba236aa692cd7db607
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-20 22:07:55 +0900

    Title: 수열의 순서 바꾸기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250320/수열의 순서 바꾸기/reorder-sequence.py
+++ 250320/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,31 +1,9 @@
 n = int(input())
-nums = list(map(int,input().split()))
 
-def check(num_list):
-    for i in range(n-1):
-        if num_list[i] > nums[i + 1]:
-            return False
-    return True
+blocks = list(map(int,input().split()))
 
+idx = n - 2
+while idx >= 0 and blocks[idx] < blocks[idx + 1]:
+    idx -= 1
 
-def shift_front(idx,num_list):
-    temp_n = num_list[0]
-    for i in range(1,idx + 1):
-        num_list[i - 1] = num_list[i]
-    num_list[idx] = temp_n
-
-count = 0 
-while True:
-    if check(nums):
-        break
-    cnt_front = nums[0]
-    for i in range(1,n):
-        if nums[i] == cnt_front -1 or cnt_front == 1 and nums[i] == n:
-            shift_front(i,nums)
-            count += 1
-            break
-    
-#     print(nums)
-# print(*nums)
-print(count)
-
+print(idx + 1)
