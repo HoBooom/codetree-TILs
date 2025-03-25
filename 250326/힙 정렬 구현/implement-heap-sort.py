16378ecffb5104a2de52a3f2f97615524b12e090
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-26 01:25:59 +0900

    Title: 힙 정렬 구현, Time: 454ms, Memory: 27MB, Status: Passed - CodeTree

--- 250326/힙 정렬 구현/implement-heap-sort.py
+++ 250326/힙 정렬 구현/implement-heap-sort.py
@@ -1,12 +1,12 @@
 n = int(input())
 
-nums = [0] + list(map(int,input().split()))
+nums = list(map(int,input().split()))
 
 def heapify(arr, n, cnt_idx):
     largest_idx = cnt_idx
 
-    left_child = cnt_idx * 2
-    right_child = cnt_idx * 2 + 1
+    left_child = cnt_idx * 2 + 1
+    right_child = cnt_idx * 2 + 2
 
     if left_child < n and arr[left_child] > arr[largest_idx]:
         largest_idx = left_child
@@ -18,13 +18,12 @@ def heapify(arr, n, cnt_idx):
         heapify(arr, n, largest_idx)
 
 def heap_sort(arr):
-    for i in reversed(range(1,n//2)):
+    for i in reversed(range(n//2 + 1)):
         heapify(arr,n,i)
 
-    for i in reversed(range(2,n)):
-        arr[1],arr[i] = arr[i],arr[1]
-        heapify(arr, i - 1, 1)
+    for i in reversed(range(1,n)):
+        arr[0],arr[i] = arr[i],arr[0]
+        heapify(arr, i, 0)
 
 heap_sort(nums)
-nums.pop(0)
 print(*nums)
