372caf8a11b1db3855defc5f477f55848d3cd45d
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-26 00:13:41 +0900

    Title: 퀵 정렬 구현, Time: 389ms, Memory: 30MB, Status: Passed - CodeTree

--- 250326/퀵 정렬 구현/implement-quick-sort.py
+++ 250326/퀵 정렬 구현/implement-quick-sort.py
@@ -1,28 +1,45 @@
-n = int(input())
+# n = int(input())
 
+# nums = list(map(int,input().split()))
+
+# def partition(arr,low,high):
+#     pivot = arr[high]
+#     i = low - 1
+
+#     for j in range(low, high):
+#         if arr[j] < pivot:
+#             i += 1
+#             arr[i],arr[j] = arr[j],arr[i]
+    
+#     arr[i + 1],arr[high] = arr[high],arr[i + 1]
+#     return i + 1
+
+# def quick_sort(arr,low,high):
+#     if low < high:
+#         pivot = partition(arr, low, high)
+
+#         quick_sort(arr,low,pivot - 1)
+#         quick_sort(arr,pivot + 1, high)
+
+
+# quick_sort(nums,0,n - 1)
+
+# print(*nums)
+
+n = int(input())
 nums = list(map(int,input().split()))
 
-def partition(arr,low,high):
-    pivot = arr[high]
-    i = low - 1
-
-    for j in range(low, high):
-        if arr[j] < pivot:
-            i += 1
-            arr[i],arr[j] = arr[j],arr[i]
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
     
-    arr[i + 1],arr[high] = arr[high],arr[i + 1]
-    return i + 1
+    pivot = arr[len(arr) // 2]
 
-def quick_sort(arr,low,high):
-    if low < high:
-        pivot = partition(arr, low, high)
+    left_arr = [x for x in arr if x < pivot]
+    middle_arr = [x for x in arr if x == pivot]
+    right_arr = [x for x in arr if x > pivot]
 
-        quick_sort(arr,low,pivot - 1)
-        quick_sort(arr,pivot + 1, high)
+    return quick_sort(left_arr) + middle_arr + quick_sort(right_arr)
 
-
-quick_sort(nums,0,n - 1)
-
+nums = quick_sort(nums)
 print(*nums)
-
