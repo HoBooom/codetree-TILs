84497c468bc96c45f490168b134d168db6051ef2
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-03-31 23:02:40 +0900

    Title: 황금비율 토스트, Time: 124ms, Memory: 20MB, Status: Passed - Codetree

--- 250331/황금비율 토스트/golden-toast.py
+++ 250331/황금비율 토스트/golden-toast.py
@@ -5,32 +5,22 @@ toast = list(input())
 cnt_idx = n
 
 def command(toast,cnt_idx,cmd, append_chr = None):
-    if cmd == "L":
-        if cnt_idx != 0:
-            cnt_idx -= 1
-    elif cmd == "R":
-        if cnt_idx != n:
-            cnt_idx += 1
-    elif cmd == "D":
-        if cnt_idx < len(toast):
-            toast = toast[:cnt_idx] + toast[cnt_idx + 1:]
+    if cmd == "L" and cnt_idx > 0:
+        return toast, cnt_idx - 1
+    elif cmd == "R" and cnt_idx < len(toast):
+        return toast, cnt_idx + 1
+    elif cmd == "D" and cnt_idx < len(toast):
+        toast.pop(cnt_idx)
+        return toast, cnt_idx
     elif cmd == "P":
-        if cnt_idx == n:
-            toast.append(append_chr)
-            cnt_idx += 1
-        else:
-            toast = toast[:cnt_idx] + [append_chr] + toast[cnt_idx:]
-            cnt_idx += 1
+        toast.insert(cnt_idx,append_chr)
+        return toast, cnt_idx + 1
     return toast,cnt_idx
 
 for _ in range(m):
     order = list(map(str,input().split()))
-    if len(order) == 1:
-        toast,cnt_idx = command(toast,cnt_idx,order[0])
-    else:
-        toast,cnt_idx = command(toast,cnt_idx,order[0],order[1])
+    toast,cnt_idx = command(toast, cnt_idx, order[0], order[1] if len(order) > 1 else None)
     
-
 print(*toast,sep="")
         
         
