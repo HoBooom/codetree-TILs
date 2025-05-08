3ffa135870976e28682e51bf039f82880e9fae76
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-05-08 18:21:24 +0900

    Title: 사각형 채우기 3, Time: 76ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250508/사각형 채우기 3/rectangle-fill-3.py
+++ 250508/사각형 채우기 3/rectangle-fill-3.py
@@ -1,11 +1,16 @@
 n = int(input())
+MOD = 1000000007
 
 dp = [0] * 1001
 
+dp[0] = 1
 dp[1] = 2
 dp[2] = 7
+#dp[3] = 22
 
-for i in range(3,n + 1):
-    dp[i] = (2 * dp[i - 1]) + (4 * dp[i - 2])
+for i in range(2,n + 1):
+    dp[i] = (2 * dp[i - 1]) + (3 * dp[i - 2]) % MOD
+    for j in range(i - 3, -1, -1):
+        dp[i] = (dp[i] + dp[j] * 2) % MDO
 
-print(dp[n] % 1000000007)
+print(dp[n])
