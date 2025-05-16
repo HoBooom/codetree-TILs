b339a4ac2faca9aadf75f9cc36c053204c7633e5
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-05-16 15:54:55 +0900

    Title: 금 채굴하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250516/금 채굴하기/gold-mining.py
@@ -0,0 +1,34 @@
+n, m = map(int,input().split())
+
+board = [list(map(int,input().split())) for _ in range(n)]
+
+def is_range(r,c):
+    return 0 <= r < n and 0 <= c < n
+
+def check(r,c,k):
+    temp_count = 0
+    for cnt_r in range(n):
+        for cnt_c in range(n):
+            if abs(r - cnt_r) + abs(c - cnt_c) <= k:
+                temp_count += board[cnt_r][cnt_c]
+    return temp_count
+
+def cost(k):
+    return k ** 2 + (k + 1) ** 2
+
+ans = 0 
+
+for r in range(n):
+    for c in range(n):
+        for k in range(2*(n-1)):
+            cnt_benefit = check(r,c,k)
+            cnt_cost = cost(k)
+            if cnt_benefit * m > cnt_cost:
+                ans = max(cnt_benefit,ans)
+            #print(f"r,c : {r},{c}  {cnt_benefit},{cnt_cost},{cnt_total}")
+            
+
+print(ans)
+
+
+            
