63623c2a921a4a5b9721cbbd2b72a392c12a9b33
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-05-28 10:01:21 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 114ms, Memory: 20MB, Status: Passed - Codetree

--- 250528/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
+++ 250528/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
@@ -1,29 +1,17 @@
 n, m = map(int,input().split())
 
-dias = []
-
-for _ in range(n):
-    w,v = map(float,input().split())
-    cost = v / w
-    dias.append([w,v,cost])
-
-dias.sort(key = lambda x : x[2])
+dias = [tuple(map(int,input().split())) for _ in range(n)]
 
 ans = 0
-cnt = 0
 
-for i in reversed(range(n)):
-    isEnd = False
-    while dias[i][0] > 0:
-        if cnt == m:
-            isEnd = True
-            break
-        ans += dias[i][2]
-        dias[i][0] -= 1
-        cnt += 1
+dias.sort(key = lambda x: -x[1]/x[0])
 
-    if isEnd:
+for w,v in dias:
+    if m >= w:
+        m -= w
+        ans += v
+    else:
+        ans += m * v / w
         break
 
 print(f"{ans:.3f}")
-
