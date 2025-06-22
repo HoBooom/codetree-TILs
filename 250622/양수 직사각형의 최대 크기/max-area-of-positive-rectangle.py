bb85a82c54a618aca699dbc4653a9aa37e0451cc
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-06-22 22:13:25 +0900

    Title: 양수 직사각형의 최대 크기, Time: 88ms, Memory: 19MB, Status: Passed - Codetree

--- 250622/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 250622/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -5,7 +5,7 @@ board = [list(map(int,input().split())) for _ in range(n)]
 def check_pos_rect(x1,y1,x2,y2):
     for x in range(x1,x2 + 1):
         for y in range(y1,y2 + 1):
-            if board[x][y] < 0:
+            if board[x][y] <= 0:
                 return False
     return True
 
