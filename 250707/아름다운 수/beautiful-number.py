cca816b65f4d76add6cbf586a92a58b69d2ae8c8
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-07-07 16:27:45 +0900

    Title: 아름다운 수, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250707/아름다운 수/beautiful-number.py
+++ 250707/아름다운 수/beautiful-number.py
@@ -3,10 +3,24 @@ N = int(input())
 count = 0
 nums = []
 
+def is_beautiful(nums):
+    i = 0
+    while i < len(nums):
+        num = nums[i]
+        if num > 4:
+            return False
+        for j in range(i,i + num):
+            if nums[j] != num:
+                return False
+        i += num
+    return True
+
+
 def make_num(n):
     global count
     if n == N + 1:
-        count += 1
+        if is_beautiful(nums):
+            count += 1
         #print(*nums)
         return
     elif n > N + 1:
