64d9e6bff68a96196ada91035e8d193e3b1702d9
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-07-10 11:34:05 +0900

    Title: 사다리 타기, Time: 74ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250710/사다리 타기/ladder-game.py
@@ -0,0 +1,97 @@
+import sys
+
+INT_MAX = sys.maxsize
+
+n,m = map(int,input().split())
+
+lines = [
+    tuple(map(int,input().split())) 
+    for _ in range(m)
+]
+
+lines.sort(key = lambda x : (x[0],x[1]))
+
+cnt_lines = []
+
+ans = INT_MAX
+
+
+def ori_output(lines):
+    ladder = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for a,b in lines:
+        ladder[b][a] = 1
+
+    ori_output = []
+    
+    for start in range(1,n + 1):
+        pos = start
+        for row in range(1,m + 1):
+            if ladder[row][pos]:
+                pos += 1
+            elif ladder[row][pos - 1]:
+                pos -= 1
+        ori_output.append(pos)
+    
+    return ori_output
+
+ori_output = ori_output(lines)
+
+
+def line_output(cnt_lines_check):
+    global ori_output
+
+    ladder = [[0] * (n + 1) for _ in range(m + 1)]
+
+    cnt_ladder_count = 0
+    
+    for i in range(len(cnt_lines_check)):
+        if cnt_lines_check[i] == 1:
+            a,b = lines[i]
+            ladder[b][a] = 1
+            cnt_ladder_count += 1
+
+    output = []
+
+    for start in range(1,n + 1):
+        pos = start
+        for row in range(1,m + 1):
+            if ladder[row][pos]:
+                pos += 1
+            elif ladder[row][pos - 1]:
+                pos -= 1
+        output.append(pos)
+
+    if output == ori_output:
+        return cnt_ladder_count
+
+    return INT_MAX
+
+def make_ladder(num):
+    global ans
+    if num == m:
+        cnt_output = line_output(cnt_lines)
+        ans = min(ans,cnt_output)
+        return 
+    
+    for i in range(2):
+        cnt_lines.append(i)
+        make_ladder(num + 1)
+        cnt_lines.pop()
+
+
+
+make_ladder(0)
+
+print(ans)
+
+    
+
+
+
+
+
+
+
+
+
