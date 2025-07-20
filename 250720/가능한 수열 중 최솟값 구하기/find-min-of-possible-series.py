a5dd7fe4762726bd63ae41ce9c2e9e99d111db8c
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-07-20 20:16:46 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 64ms, Memory: 17MB, Status: Passed - Codetree

--- 250720/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 250720/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -5,32 +5,42 @@ n = int(input())
 nums = []
 found = False
 
-def check(num):
-    for i in range(1,n + 1):
-        for j in range(n - 2 * i + 1):
+def check():
+    len_n = len(nums)
+    for i in range(1,len_n + 1):
+        for j in range(len_n - 2 * i + 1):
             if nums[j:j + i] == nums[j + i:j + 2 * i]:
                 return False
     return True
 
-def make_nums(len_n):
-    global nums
-    global found
-    if found:
+def make_nums():
+    if len(nums) == n:
+        print(*nums, sep="")
         return True
+    
+    
+        
+    # global nums
+    # global found
+    # if found:
+    #     return True
 
-    if len_n == n:
-        if check(nums):
-            print(*nums,sep="")
-            found = True
-            # nums_list.append(nums[:])
-        return
+    # if len_n == n:
+    #     if check(nums):
+    #         print(*nums,sep="")
+    #         found = True
+    #         # nums_list.append(nums[:])
+    #     return
     
     for i in range(4, 6 + 1):
         nums.append(i)
-        make_nums(len_n + 1)
+        if check():
+            if make_nums():
+                return True
         nums.pop()
+    return False
 
-make_nums(0)
+make_nums()
 
 # nums_list.sort()
 # print(*nums_list[0],sep="")
