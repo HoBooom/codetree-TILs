36e3d86dca6c25f8c61baff3603c4c284bd1d82a
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-07-27 23:28:02 +0900

    Title: 2N개 중에 N개의 숫자를 적절하게 고르기, Time: 64ms, Memory: 16MB, Status: Passed - Codetree

--- 250727/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
+++ 250727/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -7,26 +7,24 @@ n = int(input())
 nums = list(map(int,input().split()))
 
 a_nums = []
-b_nums = []
 
 ans = INT_MAX
 
-def b_select():
-    b_nums.clear()
-    for num in nums:
-        if num not in a_nums:
-            b_nums.append(num)
 
 def calc():
-    return abs(sum(a_nums) - sum(b_nums))
+    a_sum = sum(a_nums)
+    b_sum = (sum(nums) - a_sum)
+    return abs(a_sum - b_sum)
 
 
 def choose(cur_idx, cnt):
     global ans
     if cnt == n:
-        b_select()
-        #print(a_nums, b_nums)
+        cal_c = calc()
         ans = min(ans, calc())
+        # if ans > cal_c:
+        #     print(a_nums, cal_c)
+        #     ans = cal_c
     
     if cur_idx == 2 * n:
         return
