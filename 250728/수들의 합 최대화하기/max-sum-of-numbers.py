5e5b8cc51fc80d0d9143d1127ff46b6ee32cfe83
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-07-28 17:04:09 +0900

    Title: 수들의 합 최대화하기, Time: 75ms, Memory: 18MB, Status: Passed - Codetree

--- 250728/수들의 합 최대화하기/max-sum-of-numbers.py
+++ 250728/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -4,9 +4,12 @@ grid = [
     list(map(int,input().split())) for _ in range(n)
 ]
 
+max_val = max(max(nums) for nums in grid)
+
 #각 행 열에 하나씩 오려면 r도 겹치면 안되고 c도 겹치면 안됨
-visited_r = [False] * n
-visited_c = [False] * n
+# visited_r = [False] * n
+# visited_c = [False] * n
+visited = [False] * n
 
 selected_nums = []
 
@@ -16,27 +19,41 @@ def calc():
     return sum(selected_nums)
 
 def choose(cnt):
-    global ans
+    global ans, max_val
+
+    if sum(selected_nums) + (n - cnt) * max_val < ans:
+        return
 
     if cnt == n:
         ans = max(ans, calc())
         return
 
-    for r in range(n):
-        if visited_r[r]:
+    for i in range(n):
+        if visited[i]:
             continue
-        for c in range(n):
-            if visited_c[c]:
-                continue
-            
-            selected_nums.append(grid[r][c])
-            visited_r[r] = True
-            visited_c[c] = True
-            choose(cnt + 1)
+        
+        selected_nums.append(grid[cnt][i])
+        visited[i] = True
 
-            selected_nums.pop()
-            visited_r[r] = False
-            visited_c[c] = False
+        choose(cnt + 1)
+        selected_nums.pop()
+        visited[i] = False
+
+    # for r in range(n):
+    #     if visited_r[r]:
+    #         continue
+    #     for c in range(n):
+    #         if visited_c[c]:
+    #             continue
+            
+    #         selected_nums.append(grid[r][c])
+    #         visited_r[r] = True
+    #         visited_c[c] = True
+    #         choose(cnt + 1)
+
+    #         selected_nums.pop()
+    #         visited_r[r] = False
+    #         visited_c[c] = False
     
     return
 
