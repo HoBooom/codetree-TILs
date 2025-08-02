b25bc4b6a76202760d3af063d83a1e31582631db
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-08-03 00:33:15 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 628ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

---
+++ 250803/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+n,m = map(int,input().split())
+
+grid = [
+    list(map(int,input().split())) for _ in range(n)
+]
+
+visited = [
+    [False for _ in range(m)] for _ in range(n)
+]
+
+
+def is_range(r,c):
+    return 0 <= r < n and 0 <= c < m
+
+queue = deque()
+
+queue.appendleft((0,0))
+
+drs,dcs = [0,1,0,-1],[1,0,-1,0]
+
+while True:
+    if len(queue) == 0:
+        break
+    
+    cnt_r,cnt_c = queue.pop()
+    visited[cnt_r][cnt_c] = True
+
+    for dr, dc in zip(drs,dcs):
+        nr,nc = cnt_r + dr, cnt_c + dc
+        if is_range(nr,nc) and not visited[nr][nc] and grid[nr][nc] != 0:
+            queue.appendleft((nr,nc))
+
+if visited[n - 1][m - 1]:
+    print(1)
+else:
+    print(0)
+
+
+
