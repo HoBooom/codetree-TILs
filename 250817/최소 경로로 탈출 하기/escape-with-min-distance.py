b6f36891af3db74cdd0d81e7a4752c6f775adb83
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-08-17 14:05:08 +0900

    Title: 최소 경로로 탈출 하기, Time: 51ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250817/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,39 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+from collections import deque
+
+def is_range(r,c):
+    return 0 <= r < n and 0 <= c < m
+
+distance = [
+    [0 for _ in range(m)] for _ in range(n)
+]
+
+q = deque()
+
+cnt_r,cnt_c = 0,0
+
+q.append((cnt_r,cnt_c))
+
+drs,dcs = [0,1,0,-1],[1,0,-1,0]
+
+while q:
+    cnt_r, cnt_c = q.pop()
+
+    for dr,dc in zip(drs,dcs):
+        nr,nc = cnt_r + dr, cnt_c + dc
+        if is_range(nr,nc) and grid[nr][nc] == 1:
+            if distance[nr][nc] == 0:
+                distance[nr][nc] = distance[cnt_r][cnt_c] + 1
+                q.appendleft((nr,nc))
+            elif distance[nr][nc] > distance[cnt_r][cnt_c] + 1:
+                distance[nr][nc] = distance[cnt_r][cnt_c] + 1
+                q.appendleft((nr,nc))
+
+print(distance[n - 1][m - 1])
+
+
+
+
