a83a1cc32b932d48075ebaa6bf931de4280bddcc
HBChoi <144037269+HoBooom@users.noreply.github.com>
2025-08-18 11:25:10 +0900

    Title: 비를 피하기, Time: 105ms, Memory: 20MB, Status: Passed - Codetree

--- 250818/비를 피하기/stay-out-of-rain.py
+++ 250818/비를 피하기/stay-out-of-rain.py
@@ -1,70 +1,50 @@
 from collections import deque
-import sys
 
-INT_MAX = sys.maxsize
+n,h,m = map(int,input().split())
 
-n, h, m = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(n)]
+grid = [
+    list(map(int,input().split())) for _ in range(n)
+]
 
-# Please write your code here.
-
-ans_grid = [
+distance = [
     [-1 for _ in range(n)] for _ in range(n)
 ]
 
-pos_of_safe = [(r,c) for r in range(n) for c in range(n) if grid[r][c] == 3]
+drs,dcs = [0,1,0,-1],[1,0,-1,0]
+
+q = deque()
 
 def is_range(r,c):
     return 0 <= r < n and 0 <= c < n
 
-def move(r,c):
-    temp_grid = [
-        [0 for _ in range(n)] for _ in range(n)
-    ]
+for r in range(n):
+    for c in range(n):
+        if grid[r][c] == 3:
+            q.appendleft((r,c))
+            distance[r][c] = 0
 
-    drs,dcs = [0, 1, 0, -1],[1, 0, -1, 0]
+while q:
+    cnt_r,cnt_c = q.pop()
 
-    temp_min = INT_MAX
+    for dr,dc in zip(drs,dcs):
+        nr,nc = cnt_r + dr, cnt_c + dc
+        if is_range(nr,nc) and grid[nr][nc] != 1:
+            if distance[nr][nc] == -1:
+                distance[nr][nc] = distance[cnt_r][cnt_c] + 1
+                q.appendleft((nr,nc))
+            elif distance[nr][nc] > distance[cnt_r][cnt_c] + 1:
+                distance[nr][nc] = distance[cnt_r][cnt_c] + 1
+                q.appendleft((nr,nc))
 
-    q = deque()
-    q.appendleft((r,c))
-
-    while q:
-        cnt_r,cnt_c = q.pop()
-
-        for dr,dc in zip(drs,dcs):
-            nr,nc = cnt_r + dr, cnt_c + dc
-            if is_range(nr,nc) and grid[nr][nc] != 1:
-                if temp_grid[nr][nc] == 0:
-                    temp_grid[nr][nc] = temp_grid[cnt_r][cnt_c] + 1
-                    q.appendleft((nr,nc))
-                elif temp_grid[nr][nc] > temp_grid[cnt_r][cnt_c] + 1:
-                    temp_grid[nr][nc] = temp_grid[cnt_r][cnt_c] + 1
-                    q.appendleft((nr,nc))
-    
-    for (r,c) in pos_of_safe:
-        if temp_grid[r][c] != 0:
-            temp_min = min(temp_min, temp_grid[r][c])
-    
-    # print("=" *  20)
-    # print(*temp_grid, sep='\n')
-    if temp_min == INT_MAX:
-        return -1
-    return temp_min
+ans_grid = [
+    [0 for _ in range(n)] for _ in range(n)
+]
 
 for r in range(n):
     for c in range(n):
         if grid[r][c] == 2:
-            ans_grid[r][c] = move(r,c)
-            # print(ans_grid[r][c])
-        else:
-            ans_grid[r][c] = 0
+            ans_grid[r][c] = distance[r][c]
 
 for low in ans_grid:
     print(*low)
-
-
-
-    
-
 
